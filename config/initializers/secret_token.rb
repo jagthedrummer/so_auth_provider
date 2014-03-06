19abed926a2c876a382c83b6143f23cbd64a9ad6
Jeremy Green <jeremy@octolabs.com>
2014-03-05 22:08:53 -0600

    Initial commit. 'rails new so_auth_provider -d postgresql -T'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoAuthProvider::Application.config.secret_key_base = '3361d5f6345ed00fd809e8cb82fc4ae0c3bc4aa9e033272d9c4896a8d59e6837d768c941f543521b1ca776defa769fdcdb3fd7894376499693115ad14d46db1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoAuthProvider::Application.config.session_store :cookie_store, key: '_so_auth_provider_session'
